8182ac9850b34c95f1f4a36c4375587ade86f186
Devourin <dnyaneshwar@devourin.com>
2022-09-19 22:41:08 +0530

    disable manifest changes

--- app/components/modules/headMetaTags/index.tsx
+++ app/components/modules/headMetaTags/index.tsx
@@ -17,7 +17,7 @@ export default function HeadMetaTags({ title, description, image, siteName, stor
         "name": `${storeData.tenant}, ${storeData.name}` || 'Respark',
         "short_name": `${storeData.tenant}` || 'Respark',
         // "start_url": storeData.baseRouteUrl?.slice(0, -1) || '/',
-        "start_url": storeData.baseRouteUrl?.slice(0, -1) || '/',
+        "start_url": '/',
         "display": "standalone",
         "background_color": theme_color || "#dee1ec",
         "theme_color": theme_color || "#dee1ec",
